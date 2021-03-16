a2e4b90f9069ae74edca1d133623b67280cb0be1
David Stinnett <davidstinnett@icloud.com>
2021-03-16 19:41:59 -0400

    practiced with some state concepts

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
     this.state = {
       monsters: [],
       searchField: "",
+      title: "",
     };
   }
 
@@ -23,22 +24,22 @@ class App extends Component {
   }
 
   handleChange = (e) => {
-    this.setState({ searchField: e.target.value });
+    this.setState({ searchField: e.target.value, title: e.target.value });
   };
 
   render() {
-    const { monsters, searchField } = this.state;
-    const filteredMonsters = monsters.filter((monster) =>
-      monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
+    const { monsters, searchField, title } = this.state;
+    // const filteredMonsters = monsters.filter((monster) =>
+    //   monster.name.toLowerCase().includes(searchField.toLowerCase())
+    // );
     return (
       <div className="App">
-        <h1>Monsters Rolodex</h1>
+        <h1>{title}</h1>
         <SearchBox
           placeholder="search monsters"
           handleChange={this.handleChange}
         />
-        <CardList monsters={filteredMonsters}></CardList>
+        <CardList monsters={monsters}></CardList>
       </div>
     );
   }
